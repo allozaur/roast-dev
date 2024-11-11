a203d6a5b458ed5149fc059d808c66cd661c1483
Aleksander Grygier <aleksander.grygier@gmail.com>
2024-11-11 11:10:20 +0100

    chore: Remove legacy PoC plugin code

--- apps/chrome-plugin/src/background.js
+++
@@ -1,7 +0,0 @@
-// @ts-expect-error - This is a Chrome extension API
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'openPopup') {
-    // @ts-expect-error - This is a Chrome extension API
-    chrome.action.openPopup();
-  }
-});
